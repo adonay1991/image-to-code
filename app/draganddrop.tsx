2d13625140f46ed443c9c0d77941bffd1319a46e
Adonay <aguilarte@atlax360.com>
2023-12-01 20:05:32 +0100

    feat: Add drag and drop and form components to the application
    - Added new files `draganddrop.tsx` and `form.tsx`
    - Imported necessary components from `@files-ui/react`
    - Implemented `DragAndDrop` component with `updateFiles` function
    - Implemented `Form` component with form elements and event handler

---
+++ app/draganddrop.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Dropzone, ExtFile } from '@files-ui/react'
+
+export const DragAndDrop = ({ transformImageToCode }: { transformImageToCode: (file: File) => Promise<void> }) => {
+	const updateFiles = (files: ExtFile[]) => {
+		const file = files[0].file
+		if (file != null) transformImageToCode(file)
+	}
+
+	return (
+		<Dropzone
+			header={false}
+			footer={false}
+			maxFiles={1}
+			label="Arrastra aquí tu captura de pantalla"
+			accept="image/*"
+			onChange={updateFiles}
+		/>
+	)
+}
